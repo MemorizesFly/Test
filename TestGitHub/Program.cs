f937a86fad65fd10c762675cafe120ed571e3860
杨鹏飞 <956862806@qq.com>
2018-09-06 09:25:37 +0800

    小牛项目的创建

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -10,9 +10,43 @@ namespace TestGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is first time i getting into the world of github");
-            Console.WriteLine("The author has been changed ");
-            Console.WriteLine("测试");
+            Console.WriteLine("请输入账号");
+            string UserName = Console.ReadLine();
+            Console.WriteLine("请输入密码");
+            string UserPassword = Console.ReadLine();
+            testInterface test = new testInterface();
+            test.login(UserName, UserPassword);
+            Console.WriteLine("请输入当前状态");
+            string statusCode = Console.ReadLine();
+            Console.WriteLine(test.getStatus(statusCode)); 
+
+        }
+    }
+
+    public interface ILoginEvent
+    {
+        void login(string UserName,string UserPassword);
+        bool getStatus(string StatusCode);
+        
+    }
+
+    class testInterface : ILoginEvent
+    {
+        public bool getStatus(string StatusCode)
+        {
+            if (StatusCode=="true")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void login(string UserName, string UserPassword)
+        {
+            if (UserName=="杨鹏飞"&&UserPassword=="123456")
+            {
+                Console.WriteLine("登陆成功");
+            }
         }
     }
 }
